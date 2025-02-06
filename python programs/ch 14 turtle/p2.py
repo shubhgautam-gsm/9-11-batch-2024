7f7158a7253d60fd7bf72ee3d8ecc23911595de5
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-02-06 12:49:57 +0530

    6-2-25

---
+++ python programs/ch 14 turtle/p2.py
@@ -0,0 +1,43 @@
+import  turtle 
+# Set up the turtle
+my_window = turtle.Screen() 
+my_window.bgcolor("blue") # creates a graphics window
+my_pen = turtle.Turtle()
+my_pen2 = turtle.Turtle()
+
+#my_pen.shape('turtle')
+my_pen.color("red")
+my_pen.pensize(30)
+my_pen.forward(250) 
+my_pen.left(90) 
+my_pen.forward(300)
+
+my_pen.right(90) 
+my_pen.forward(100)
+
+
+my_pen2.color("green")
+my_pen2.pensize(30)
+my_pen2.penup()
+my_pen2.forward(289) #0.05
+my_pen2.pendown()
+my_pen2.left(90) 
+my_pen2.forward(80)
+
+my_pen2.left(90) 
+my_pen2.forward(100)
+
+my_pen2.right(90) 
+my_pen2.forward(100)
+
+
+
+
+
+
+
+
+
+# o.virus(x,y)
+
+
